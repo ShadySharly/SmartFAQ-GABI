58a7216be605d09fe520d32a800e464e071e0205
Carlos <carlos.perez.sa@usach.cl>
2021-05-29 21:57:27 -0400

    Create tables for Query, Message, Intention, Question and Answer

--- Back/database/database.sql
+++ Back/database/database.sql
@@ -35,3 +35,52 @@ CREATE TABLE permission_duty (
     REFERENCES permission(permission_id)    
 );
 
+CREATE TABLE query (
+    query_id int NOT NULL IDENTITY(1, 1),
+    user_id int
+    content varchar(256)
+    PRIMARY KEY(query_id)
+    CONSTRAINT fk_user
+        FOREIGN KEY(user_id)
+        REFERENCES user(user_id)
+)
+
+CREATE TABLE message (
+    message_id int NOT NULL IDENTITY(1, 1),
+    conversation_id int 
+    intention_id int
+    content varchar(256)
+    confidence int
+    date_issue date
+    PRIMARY KEY(message_id)
+    CONSTRAINT fk_conversation
+        FOREIGN KEY(conversation_id)
+        REFERENCES conversation(conversation_id),
+    CONSTRAINT fk_intention
+        FOREIGN KEY(intention_id)
+        REFERENCES conversation(intention_id)
+)
+
+CREATE TABLE intention (
+    intention_id int NOT NULL IDENTITY(1, 1),
+    name varchar(32)
+    PRIMARY KEY(intention_id)
+)
+
+CREATE TABLE question (
+    question_id int NOT NULL IDENTITY(1, 1),
+    intention_id int 
+    content varchar(256)
+    PRIMARY KEY(question_id)
+    CONSTRAINT fk_intention
+        FOREIGN KEY(intention_id)
+        REFERENCES intention(intention_id)
+)
+
+CREATE TABLE answer (
+    answer_id int NOT NULL IDENTITY(1, 1),
+    content varchar(256)
+    image_url varchar(64)
+    video_url varchar(64)
+    PRIMARY KEY(answer_id)
+)
