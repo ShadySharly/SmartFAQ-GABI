51ec1b562a70d85dccc88e542d519664f70ecc51
Matias <matias.coronado@usach.cl>
2021-06-16 00:47:42 -0400

    fix: Get userquestion data

--- Front/chatbot-app/src/Views/QueryAssignment.tsx
+++ Front/chatbot-app/src/Views/QueryAssignment.tsx
@@ -3,9 +3,11 @@ import { makeStyles } from '@material-ui/core/styles';
 import {Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper} from '@material-ui/core'
 import { BiSend } from 'react-icons/bi';
 import { useQuery, gql } from '@apollo/client';
+import { useMutation } from '@apollo/react-hooks';
 import Select from "react-dropdown-select";
 import Dropdown from 'react-dropdown';
 import 'react-dropdown/style.css';
+import styled from 'styled-components';
 
 const useStyles = makeStyles({
   table: {
@@ -13,33 +15,60 @@ const useStyles = makeStyles({
   },
 });
 
+const ButtonIcon = styled(Link)`
+  display:flex;
+  jusfity-content: flex-start;
+  align-items: center;
+  height: 3rem;
+  font-size: 2rem;
+  margin-left: 2rem;
+`
 
-const INTENTIONS = gql`
+const QUERY = gql`
   query consulta {
     intentions{
+      intention_id
       intention_name
     }
+    userquestions{
+      userquestion_id
+      information
+      intention{
+        intention_id
+        intention_name
+      }
+    }    
   }
 `;
+
+
+const UPDATE_USER = gql`
+  mutation consulta {
+    updateUserquestion($userquestion_id: Int!, $intention_id: Int!)
+  }
+`
+
 type Intention = {
   intention_id: number,
   intention_name: string
 }
 
-const rows = [
-  {query:'consulta',userquestion_id:'1',information:'¿Como puedo derivar?'},
-  {query:'consulta',userquestion_id:'2',information:'¿Como puedo multiplicar matrices?'},
-  {query:'consulta',userquestion_id:'3',information:'¿Como puedo multiplicar matrices?'},
-];
-
-interface IntentionVars {
-  intention_name: string;
+type Userquestion = {
+  userquestion_id: number,
+  information: string,
+  intention: Intention
 }
+
+
 export default function QueryAssignment() {
   const classes = useStyles();
-  const {loading, error, data } = useQuery(INTENTIONS);
+  const {loading, error, data } = useQuery(QUERY);
+  const [updateUser] = useMutation(UPDATE_USER);
   const [dropdown, setDropdown] = useState(false);
   const dropdownIsOpen = () => setDropdown(!dropdown);
+
+  //updateUser({ variables: {userquestion_id:this.userquestion_id ,intention_id:this.intention_id}});
+
   var intencionElegida:any  = null
   if (loading) return <p>Loading...</p>;
   if (error) return <p>Error :(</p>;
@@ -55,10 +84,10 @@ export default function QueryAssignment() {
           </TableRow>
         </TableHead>
         <TableBody>
-          {rows.map((row) => (
-            <TableRow key={row.query}>
-              <TableCell component="th" scope="row">{row.query}</TableCell>
-              <TableCell align="right">{row.query}
+          {data.userquestions.map((userquestion:Userquestion) => (
+            <TableRow key={userquestion.userquestion_id}>
+              <TableCell component="th" scope="row">{userquestion.information}</TableCell>
+              <TableCell align="right">{userquestion.userquestion_id}
                 <select name="intentionSelection" onChange={intencionElegida}>
                   {data.intentions.map((intention:Intention) => (
                     <option key={intention.intention_id} value={intention.intention_name}>
@@ -67,8 +96,12 @@ export default function QueryAssignment() {
                   ))}
                 </select>       
               </TableCell>
-              <TableCell align="right"> <BiSend/> </TableCell>
+              <TableCell align="right">
+                <Butt>
+                  <BiSend/> 
+              </TableCell>
             </TableRow>
+            
           ))}
         </TableBody>
       </Table>
