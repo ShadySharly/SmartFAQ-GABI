a5484cf453b98d9c90a7ded20540b835fa4c2e72
Matias <matias.coronado@usach.cl>
2021-07-06 16:52:22 -0400

    Fix: mayor problem in generation of rasa files

--- Apollo-Server/graphql/chatbotfunctions.js
+++ Apollo-Server/graphql/chatbotfunctions.js
@@ -5,10 +5,10 @@ function generateNLU (requests, chatbot_version){
     let auxIntention = 'null'
     for(let i=0 ; i < requests.length ; i++){
         if(requests[i]['intention_name'] == auxIntention){
-            nlu_content = nlu_content + "\t\t- "+requests[i]['information']+"\n"
+            nlu_content = nlu_content + "   - "+requests[i]['information']+"\n"
         }else{
             auxIntention = requests[i]['intention_name']
-            nlu_content = nlu_content + "- intent: "+auxIntention+"\n \texamples: |\n"
+            nlu_content = nlu_content + "- intent: "+auxIntention+"\n  examples: |\n"
             i = i - 1
         }
     }
@@ -18,16 +18,40 @@ function generateNLU (requests, chatbot_version){
 function generateRules (routine_intention, chatbot_version){
     rules_content = "version: \""+chatbot_version+"\"\nrules: \n";
     let auxTitle = 'null'
+    let auxValues = null
     for(let i=0 ; i < routine_intention.length ; i++){
         if(routine_intention[i]['type'] == 'rule'){
             if(routine_intention[i]['title'] == auxTitle){
-                rules_content = rules_content + "\t- "+routine_intention[i]['step_labbel']+": "+routine_intention[i]['intention_name']+"\n"
+                auxValues.push(routine_intention[i])
+                //rules_content = rules_content + "  - "+routine_intention[i]['step_labbel']+": "+routine_intention[i]['intention_name']+"\n"
             }else{
+                if(auxValues!=null){
+                    auxValues.sort(function(a,b){
+                        if(a['step_order'] > b['step_order']){
+                            return 1;
+                        }
+                        if(a['step_order'] < b['step_order']){
+                            return -1;
+                        }else{
+                            return 0;
+                        }
+                    })
+                    for(let j = 0; j < auxValues.length;j++){
+                        rules_content = rules_content + "  - "+auxValues[j]['step_labbel']+": "+auxValues[j]['intention_name']+"\n"
+                    }
+                    auxValues = new Array();
+                }else{
+                    auxValues = new Array();
+                }
                 auxTitle = routine_intention[i]['title']
-                rules_content = rules_content + "- rule: "+auxTitle+"\n \tsteps:\n"
+                rules_content = rules_content + "- rule: "+auxTitle+"\n  steps:\n"
                 i = i - 1
+                //Procerso que guarda los pasos
             }  
         } 
+    }
+    for(let j = 0; j < auxValues.length;j++){
+        rules_content = rules_content + "  - "+auxValues[j]['step_labbel']+": "+auxValues[j]['intention_name']+"\n"
     }
     return rules_content 
 }
@@ -35,24 +59,46 @@ function generateRules (routine_intention, chatbot_version){
 function generateStories(routine_intention,chatbot_version){
     stories_content = "version: \""+ chatbot_version +"\"\nstories: \n";
     let auxTitle = 'null'
+    let auxValues = null
     for(let i=0 ; i < routine_intention.length ; i++){    
         if(routine_intention[i]['type'] == 'story'){
             if(routine_intention[i]['title'] == auxTitle){
-                stories_content = stories_content + "\t- "+routine_intention[i]['step_labbel']+": "+routine_intention[i]['intention_name']+"\n"
+                auxValues.push(routine_intention[i])
             }else{
+                if(auxValues!=null){
+                    auxValues.sort(function(a,b){
+                        if(a['step_order'] > b['step_order']){
+                            return 1;
+                        }
+                        if(a['step_order'] < b['step_order']){
+                            return -1;
+                        }else{
+                            return 0;
+                        }
+                    })
+                    for(let j = 0; j < auxValues.length;j++){
+                        stories_content = stories_content + "  - "+auxValues[j]['step_labbel']+": "+auxValues[j]['intention_name']+"\n"
+                    }
+                    auxValues = new Array();
+                }else{
+                    auxValues = new Array();
+                }
                 auxTitle = routine_intention[i]['title']
-                stories_content = stories_content + "- story: "+auxTitle+"\n \tsteps:\n"
+                stories_content = stories_content + "- story: "+auxTitle+"\n  steps:\n"
                 i = i - 1
             }
         }
+    }
+    for(let j = 0; j < auxValues.length;j++){
+        stories_content = stories_content + "  - "+auxValues[j]['step_labbel']+": "+auxValues[j]['intention_name']+"\n"
     }
     return stories_content    
 }
 
 function generateDomain_addIntention(intentions,chatbot_version){
-    domain_content = "version: \""+ chatbot_version +"\"\nconfig: \n\tstore_entities_as_slots: true\nintents:\n";    
+    domain_content = "version: \""+ chatbot_version +"\"\nconfig: \n store_entities_as_slots: true\nintents:\n";    
     for (let i = 0; i < intentions.length ; i++){
-        domain_content = domain_content + "\t- "+intentions[i]['intention_name']+"\n"
+        domain_content = domain_content + " - "+intentions[i]['intention_name']+"\n"
     }
     return domain_content
 }
@@ -61,11 +107,11 @@ function generateDomain_addIntention(intentions,chatbot_version){
 function generateDomain_addslots(slots, domain_content){
     domain_content = domain_content + "entities:\n"
     for(let i = 0 ; i < slots.length ; i++){
-        domain_content = domain_content + "\t - "+slots[i]['content']+"\n"
+        domain_content = domain_content + "  - "+slots[i]['content']+"\n"
     }                
     domain_content = domain_content + "slots:\n"
     for(let i = 0 ; i < slots.length ; i++){
-        domain_content = domain_content + "\t"+slots[i]['content']+":\n\t\ttype: "+slots[i]['slot_type']+"\n"
+        domain_content = domain_content + " "+slots[i]['content']+":\n  type: "+slots[i]['slot_type']+"\n"
     }
     domain_content = domain_content + "responses:\n"
     return domain_content
@@ -76,22 +122,23 @@ function generateDomain_addRWithB(answer_withButtons, domain_content){
     let auxIntention = 'null'
     for(let i = 0 ; i < answer_withButtons.length ; i++){
         if(answer_withButtons[i]['intention_name'] == auxIntention){
-            domain_content = domain_content + "\t\t- payload: "+answer_withButtons[i]['payload']+"\n\t\t\ttitle: "+answer_withButtons[i]['title']+"\n"
+            domain_content = domain_content + "    - payload: "+answer_withButtons[i]['payload']+"\n      title: "+answer_withButtons[i]['title']+"\n"
         }else{
             if(auxIntention != 'null'){
-                domain_content = domain_content + "\t\ttext: "+answer_withButtons[i-1]['information']+"\n"
+                domain_content = domain_content + "   text: "+answer_withButtons[i-1]['information']+"\n"
             }
             auxIntention = answer_withButtons[i]['intention_name']
-            domain_content = domain_content+"\t"+auxIntention+":\n\t- buttons:\n"
+            domain_content = domain_content+" "+auxIntention+":\n - buttons:\n"
             i = i - 1
         }
     }
+    domain_content = domain_content + "   text: "+answer_withButtons[answer_withButtons.length-1]['information']+"\n"
     return domain_content
 }
 
 function generateDomain_addRWithoutB(answer_withoutButtons, domain_content){
     for(let i=0 ; i < answer_withoutButtons.length ; i++){
-        domain_content = domain_content +"\t"+ answer_withoutButtons[i]['intention_name']+":\n\t\ttext: "+answer_withoutButtons[i]['information']+"\n"
+        domain_content = domain_content +" "+ answer_withoutButtons[i]['intention_name']+":\n  - text: "+answer_withoutButtons[i]['information']+"\n"
     }       
     return domain_content
 }
@@ -99,9 +146,9 @@ function generateDomain_addRWithoutB(answer_withoutButtons, domain_content){
 function generateDomain_addActions(actions, domain_content){
     domain_content = domain_content + "actions:\n"
     for(let i=0 ; i < actions.length;i++){
-        domain_content = domain_content + "\t- "+actions[i]['intention_name']+"\n"
+        domain_content = domain_content + " - "+actions[i]['intention_name']+"\n"
     }
-    return domain_content + "forms: {}\ne2e_actions: []\nsesion_config:\n\tsession_expiration_time: 60\n\tcarry_over_slots_to_new_session: true"
+    return domain_content + "forms: {}\ne2e_actions: []\nsesion_config:\n session_expiration_time: 60\n carry_over_slots_to_new_session: true"
 }
 
 function generateFiles(name, content){
