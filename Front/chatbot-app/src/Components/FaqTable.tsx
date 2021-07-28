66aee44897b3a106d6b657899495d0398de1458c
Vani <linda.munoz@usach.cl>
2021-07-28 19:15:49 -0400

    Style: faqindex and faqtable style

--- Front/chatbot-app/src/Components/FaqTable.tsx
+++ Front/chatbot-app/src/Components/FaqTable.tsx
@@ -1,84 +1,22 @@
 import React, { useState } from 'react';
 import { useQuery, gql, useMutation } from '@apollo/client';
-import { Theme, createStyles, makeStyles } from '@material-ui/core/styles';
-import clsx from 'clsx';
-import Accordion from '@material-ui/core/Accordion';
 import AccordionDetails from '@material-ui/core/AccordionDetails';
-import AccordionSummary from '@material-ui/core/AccordionSummary';
-import AccordionActions from '@material-ui/core/AccordionActions';
 import Typography from '@material-ui/core/Typography';
-import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
-import Chip from '@material-ui/core/Chip';
-import Button from '@material-ui/core/Button';
-import Divider from '@material-ui/core/Divider';
-import EditRequestForm from './EditRequestForm';
-import AddRequestForm from './AddRequestForm';
-import AddIcon from '@material-ui/icons/Add';
-import DoneIcon from '@material-ui/icons/Done';
-
-
-import List from '@material-ui/core/List';
-import ListItem from '@material-ui/core/ListItem';
-import ListItemAvatar from '@material-ui/core/ListItemAvatar';
-import ListItemIcon from '@material-ui/core/ListItemIcon';
-import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction';
-import ListItemText from '@material-ui/core/ListItemText';
-import Avatar from '@material-ui/core/Avatar';
 import IconButton from '@material-ui/core/IconButton';
-import FormGroup from '@material-ui/core/FormGroup';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import Checkbox from '@material-ui/core/Checkbox';
-import Grid from '@material-ui/core/Grid';
-import FolderIcon from '@material-ui/icons/Folder';
-import DeleteIcon from '@material-ui/icons/Delete';
-import EditIcon from '@material-ui/icons/Edit';
+import {RiDeleteBin6Fill,RiEdit2Line} from 'react-icons/ri';
+import styled from 'styled-components';
 
+const StyledDiv = styled.div`
+  .MuiAccordionDetails-root{
+    display:block;
+    text-align:left;
+  };
+  .Icons{
+    align-items:left;
+  };
 
-const useStyles = makeStyles((theme: Theme) =>
-  createStyles({
-    root: {
-      width: '100%',
-    },
-    heading: {
-      fontSize: theme.typography.pxToRem(15),
-    },
-    secondaryHeading: {
-      fontSize: theme.typography.pxToRem(15),
-      color: theme.palette.text.primary,
-    },
-    icon: {
-      verticalAlign: 'bottom',
-      height: 20,
-      width: 20,
-    },
-    details: {
-      alignItems: 'center',
-    },
-    column: {
-      flexBasis: '33.33%',
-    },
-    helper: {
-      borderLeft: `2px solid ${theme.palette.divider}`,
-      padding: theme.spacing(1, 2),
-    },
-    link: {
-      color: theme.palette.primary.main,
-      textDecoration: 'none',
-      '&:hover': {
-        textDecoration: 'underline',
-      },
-    },
-    button: {
-      margin: theme.spacing(1),
-    },
-    margin: {
-      margin: theme.spacing(1),
-    },
-    extendedIcon: {
-      marginRight: theme.spacing(1),
-    },
-  }),
-);
+`
+
 
 const REQUESTS_BY_INTENT = gql`
   query getRequests ($intention_id: Int!) {
@@ -104,7 +42,6 @@ interface IProps {
 }
 
 const FaqTable: React.FunctionComponent<IProps> = props => {
-  const classes = useStyles();
   const [deleteRequest] = useMutation(DELETE_REQUEST)
   const { loading, error, data } = useQuery(REQUESTS_BY_INTENT, {
     variables: { intention_id: props.intent.intention_id }, pollInterval: 500,
@@ -118,22 +55,21 @@ const FaqTable: React.FunctionComponent<IProps> = props => {
   if (error) return <p>ERROR</p>;
 
   return (
-    <div className={classes.root}>
+    <StyledDiv >
 
       {data.requestByIntent.length > 0 ? (
         data.requestByIntent.map((r: Request) => (
-          <AccordionDetails className={classes.details}>
-            <div className={classes.column} />
-            <div className={classes.column}>
+          <AccordionDetails>
+            <div className="Faq">
               {r.information}
             </div>
-            <div className={clsx(classes.column, classes.helper)}>
+            <div className="Icons">
               <Typography variant="caption">
-                  <IconButton aria-label="edit" className={classes.margin}>
-                    <EditIcon fontSize="large" />
+                  <IconButton aria-label="edit">
+                    <RiEdit2Line style={{fill:"#043C8B", fontSize:"20px"}} />
                   </IconButton>
-                  <IconButton aria-label="delete" className={classes.margin}>
-                    <DeleteIcon fontSize="large" onClick={() => { onDeleteRequest(r) }}/>
+                  <IconButton aria-label="delete">
+                    <RiDeleteBin6Fill style={{fontSize:"20px"}} onClick={() => { onDeleteRequest(r) }}/>
                   </IconButton>
               </Typography>
             </div>
@@ -144,7 +80,7 @@ const FaqTable: React.FunctionComponent<IProps> = props => {
           <td colSpan={3}>No hay Preguntas Frecuentes aún</td>
         </tr>
       )}
-    </div>
+    </StyledDiv>
   )
 }
 
--- Front/chatbot-app/src/Views/FaqIndex.tsx
+++ Front/chatbot-app/src/Views/FaqIndex.tsx
@@ -1,62 +1,23 @@
 import React, { useState } from 'react';
 import { useQuery, gql, useMutation } from '@apollo/client';
-import { Theme, createStyles, makeStyles } from '@material-ui/core/styles';
-import clsx from 'clsx';
 import Accordion from '@material-ui/core/Accordion';
 import AccordionDetails from '@material-ui/core/AccordionDetails';
 import AccordionSummary from '@material-ui/core/AccordionSummary';
-import AccordionActions from '@material-ui/core/AccordionActions';
 import Typography from '@material-ui/core/Typography';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
-import Chip from '@material-ui/core/Chip';
-import Button from '@material-ui/core/Button';
-import Divider from '@material-ui/core/Divider';
 import FaqTable from '../Components/FaqTable';
 import styled from 'styled-components';
 
-const useStyles = makeStyles((theme: Theme) =>
-  createStyles({
-    root: {
-      width: '100%',
-    },
-    heading: {
-      fontSize: theme.typography.pxToRem(15),
-    },
-    secondaryHeading: {
-      fontSize: theme.typography.pxToRem(15),
-      color: theme.palette.text.secondary,
-    },
-    icon: {
-      verticalAlign: 'bottom',
-      height: 20,
-      width: 20,
-    },
-    details: {
-      alignItems: 'center',
-    },
-    column: {
-      flexBasis: '33.33%',
-    },
-    helper: {
-      borderLeft: `2px solid ${theme.palette.divider}`,
-      padding: theme.spacing(1, 2),
-    },
-    link: {
-      color: theme.palette.primary.main,
-      textDecoration: 'none',
-      '&:hover': {
-        textDecoration: 'underline',
-      },
-    },
-  }),
-);
 
 const StyledDiv = styled.div`
   padding: 20px;
   span{
     color: #EA7600;
-    font-size:18px;
+    font-size:30px;
   };
+  .Faq {
+    margin-top:20px;
+  }
 `
 
 const INTENTIONS = gql`
@@ -65,13 +26,12 @@ const INTENTIONS = gql`
       intention_id
       intention_name
     }
-  }
+  },
 `;
 
 type Intention = { intention_id: number, intention_name: string }
 
 function FaqIndex() {
-  const classes = useStyles();
   const { loading, error, data } = useQuery(INTENTIONS);
   const [expanded, setExpanded] = React.useState<string | false>(false);
 
@@ -87,7 +47,7 @@ function FaqIndex() {
   return (
     <StyledDiv>
       <span>Preguntas Frecuentes</span>
-      <div className={classes.root}>
+      <div className="Faq">
         {data.intentions.length > 0 ? (
           data.intentions.map((i: Intention) => (
             <Accordion expanded={expanded === i.intention_name} onChange={handleChange(i.intention_name)} TransitionProps={{ unmountOnExit: true }}>
@@ -96,11 +56,8 @@ function FaqIndex() {
                 aria-controls="panel1bh-content"
                 id="panel1bh-header"
               >
-                <div className={classes.column}>
-                  <Typography className={classes.heading}>{i.intention_name}</Typography>
-                </div>
-                <div className={classes.column}>
-                  <Typography className={classes.secondaryHeading}>Preguntas asociadas</Typography>
+                <div>
+                  <Typography>{i.intention_name}</Typography>
                 </div>
               </AccordionSummary>
               <AccordionDetails>
